3200cfb874cd7147bd4c5ae22b49c67c2c31e838
Morteza Khazaei <mortezakhazaei1370@gmail.com>
2024-12-01 10:20:30 -0600

    new A and B calculation

--- src/wcmi/core.py
+++ src/wcmi/core.py
@@ -314,7 +314,7 @@ class VegParamCal:
                 categorized_angle_Avv_mean = dict(map(lambda el: (el[0], np.array(el[1]).mean()), categorized_angle_Avv.items()))
                 categorized_angle_Bvv_mean = dict(map(lambda el: (el[0], np.array(el[1]).mean()), categorized_angle_Bvv.items()))
                 
-                merged_angle_vv_soils_mvs = self.mergeDictionary(categorized_angle_mvs, categorized_angle_vv_soil)
+                merged_angle_vv_soils_mvs = dict(self.mergeDictionary(categorized_angle_mvs, categorized_angle_vv_soil))
                 # categorized_angle_Cvv_Dvv = dict(map(lambda el: (el[0], curve_fit(self.exp_func, np.array(el[1][0]), np.array(el[1][1]))), 
                 #     merged_angle_vv_soils_mvs_arr.items()))
                 wcm_param_doy[day_of_year] = [categorized_angle_Avv_mean, categorized_angle_Bvv_mean, merged_angle_vv_soils_mvs.values()]
