b6a548e8c09d95c87d126489d0007681fba808d7
Morteza Khazaei <mortezakhazaei1370@gmail.com>
2024-12-16 12:52:14 -0600

    update dict merge function

--- src/wcmi/core.py
+++ src/wcmi/core.py
@@ -474,7 +474,7 @@ class VegParamCal:
                         
                     else:
                         wcm_params = default_wcm_params[day_of_year][nearest_int_angle]
-                        A_init, B_init, Cvv_Dvv, ssm, ssr = wcm_params
+                        A_init, B_init, Cvv, Dvv, ssm, ssr = wcm_params
 
                     ssm_max = ssm + ssm_inv_thr
                     ssm_min = ssm - ssm_inv_thr
