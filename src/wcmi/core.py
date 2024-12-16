35e172d20aec888e4af0d831e42691db4fdc8677
Morteza Khazaei <mortezakhazaei1370@gmail.com>
2024-12-16 13:21:16 -0600

    update dict merge function

--- src/wcmi/core.py
+++ src/wcmi/core.py
@@ -177,7 +177,6 @@ class VegParamCal:
 
         # drop constants
         df_melted.drop(columns=['constants'], inplace=True)
-        # df_melted.head()
 
         # melt df based on angle
         df_melted = df_melted.melt(id_vars=['CropType', 'RISMA', 'depth (cm)', 'doy'], var_name='angle (deg)', value_name='constants')
@@ -185,14 +184,10 @@ class VegParamCal:
         # Expand df_melted base on constants column
         df_melted = df_melted.join(df_melted['constants'].apply(pd.Series))
 
-        df_melted['Avv'] = df_melted[0].apply(lambda x: x[0][0] if isinstance(x, list) else None)
-        df_melted['Bvv'] = df_melted[0].apply(lambda x: x[0][1] if isinstance(x, list) else None)
-        df_melted['Cvv'] = df_melted[0].apply(lambda x: x[1][0] if isinstance(x, list) else None)
-        df_melted['Dvv'] = df_melted[0].apply(lambda x: x[1][1] if isinstance(x, list) else None)
-        df_melted['ssm'] = df_melted[1].apply(lambda x: x[0] if isinstance(x, list) else None)
-        df_melted['ssr'] = df_melted[1].apply(lambda x: x[1] if isinstance(x, list) else None)
+        # rename 0, 1, 2, 3, 4, 5 to Avv, Bvv, Cvv, Dvv, ssm, ssr
+        df_melted.rename(columns={0: 'Avv', 1: 'Bvv', 2: 'Cvv', 3: 'Dvv', 4: 'ssm', 5: 'ssr'}, inplace=True)
 
-        df_melted.drop(columns=['constants', 0, 1], inplace=True)
+        df_melted.drop(columns=['constants', ], inplace=True)
 
         df_melted['angle'] = df_melted['angle (deg)'].astype(int)
 
@@ -522,6 +517,7 @@ class VegParamCal:
                 categorized_angle_ssr_mean = dict(map(lambda el: (el[0], np.median(el[1])), categorized_angle_ssr.items()))
 
                 merged_angle_vv_soils_mvs = self.merge_dicts(categorized_angle_mvs, categorized_angle_vv_soil)
+                print(merged_angle_vv_soils_mvs))
                 merged_angle_Cvv_Dvv = dict(map(lambda el: (el[0], self.curve_fit_Cvv_Dvv(el[1][0], el[1][1])), 
                     merged_angle_vv_soils_mvs.items()))
                 
