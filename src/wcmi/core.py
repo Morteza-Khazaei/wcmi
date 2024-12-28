154dea7a69d80d84dce2cbaca1ac8dfd3a6d921e
Morteza Khazaei <mortezakhazaei1370@gmail.com>
2024-12-28 15:54:09 -0600

    Doing the inversion bands on ssm

--- src/wcmi/core.py
+++ src/wcmi/core.py
@@ -601,9 +601,9 @@ class VegParamCal:
                     # sigma_tot = sigma_veg + (tau * sigma_soil)
                     sigma_soil = (vv - sigma_veg) / tau
 
-                    res = differential_evolution(self.residuals_global, bounds=[(0.01, 3)], args=(ssm, sigma_soil, theta_rad))
-                    # res = least_squares(self.residuals_global, [ssm, ssr], args=(sigma_soil, theta_rad), 
-                    #     bounds=([ssm - 0.05, 0.001], [ssm + 0.05, 5]))
+                    # res = differential_evolution(self.residuals_global, bounds=[(0.01, 3)], args=(ssm, sigma_soil, theta_rad))
+                    res = least_squares(self.residuals_global, [ssr, ], args=(ssm, sigma_soil, theta_rad), 
+                        bounds=([ssr - 0.1, ], [ssr + 0.1,]))
                     mv = res.x[0]
 
                     categorized_angle_mvs[nearest_int_angle].append(mv)
